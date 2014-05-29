eabdab618c06e2db8cb3463bf8ae0c45461c7bcc
Carlo Ilagan <cilagan@gmail.com>
2014-05-29 17:46:58 -0400

    testing stock module

--- python/test_program.py
+++ python/test_program.py
@@ -1,1 +1,14 @@
-print("this is another test file.")
+import stocks
+import datetime
+
+print("*** Getting Stock Data ***")
+print str(datetime.datetime.now())
+print ""
+
+print stocks.get_all("GOOG")
+
+print "***"
+
+print stocks.get_historical_prices("GOOG", "20131001", "20140529")
+
+print("***")
